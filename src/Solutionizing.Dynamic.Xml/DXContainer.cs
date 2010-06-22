221dca2ab1145c2262f0484197a3d5e7dfe96829
Keith Dahlby <dahlbyk@gmail.com>
2010-06-21 21:56:46 -0500

    Extract DXObject<T>

--- src/Solutionizing.Dynamic.Xml/DXContainer.cs
+++ src/Solutionizing.Dynamic.Xml/DXContainer.cs
@@ -6,45 +6,23 @@ using System.Xml.Linq;
 
 namespace Solutionizing.Dynamic.Xml
 {
-    public abstract class DXContainer<T> : DynamicObject where T : XContainer
+    public abstract class DXContainer<T> : DXObject<T> where T : XContainer
     {
-        private readonly T element;
-
-        public DXContainer(T element)
+        public DXContainer(T element) : base(element)
         {
-            this.element = element;
         }
 
         public override IEnumerable<string> GetDynamicMemberNames()
         {
-            return element == null ? Enumerable.Empty<string>() :
-                from e in element.Elements()
+            return inner == null ? Enumerable.Empty<string>() :
+                from e in inner.Elements()
                 select e.Name.LocalName;
         }
 
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
-            result = element == null ? null : element.Element(binder.Name).AsDynamic();
-            return result != null;
-        }
-
-        public override bool TryConvert(ConvertBinder binder, out object result)
-        {
-            if (binder.Type.IsAssignableFrom(typeof(T)))
-            {
-                result = element;
-                return true;
-            }
-
-            var converter = GetConverterForType(binder.Type);
-
-            if (converter == null)
-                return base.TryConvert(binder, out result);
-
-            result = converter(element);
+            result = (inner == null ? null : inner.Element(binder.Name)).AsDynamic();
             return true;
         }
-
-        protected abstract Func<T, object> GetConverterForType(Type type);
     }
 }
---
+++ src/Solutionizing.Dynamic.Xml/DXObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+using System.Xml.Linq;
+
+namespace Solutionizing.Dynamic.Xml
+{
+    public abstract class DXObject<T> : DynamicObject, IEquatable<DXObject<T>>, IEquatable<T> where T : XObject
+    {
+        protected T inner;
+
+        protected DXObject(T inner)
+        {
+            this.inner = inner;
+        }
+
+        public override bool TryConvert(ConvertBinder binder, out object result)
+        {
+            if (binder.Type.IsAssignableFrom(typeof(T)))
+            {
+                result = inner;
+                return true;
+            }
+
+            var converter = GetConverterForType(binder.Type);
+
+            if (converter == null)
+                return base.TryConvert(binder, out result);
+
+            result = converter(inner);
+            return true;
+        }
+
+        protected abstract Func<T, object> GetConverterForType(Type type);
+
+        public bool Equals(DXObject<T> other)
+        {
+            return other == null ? inner == null : Equals(other.inner);
+        }
+
+        public bool Equals(T other)
+        {
+            return other == null ? inner == null : other.Equals(inner);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is T)
+                return Equals((T)obj);
+            if (obj is DXObject<T>)
+                return Equals((DXObject<T>)obj);
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return inner == null ? 0 : inner.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return inner == null ? "" : inner.ToString();
+        }
+    }
+}
